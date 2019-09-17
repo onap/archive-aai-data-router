1a7e8802abcaa97632c46e8544d7264ce1414d06
rajeevme <rajeev.mehta@amdocs.com>
2019-09-17 15:39:13 +0530

    [AAI-2176] Run container process as non-root
    Issue-ID: AAI-2176
    
    Change-Id: Ie852a965be765d3ab24c30f692ceed278c32f874
    
    Signed-off-by: rajeevme<rajeev.mehta@amdocs.com>
    Change-Id: Iab79506c332dafb524bfe73c7e7ce9f888e53e4a

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -14,6 +14,30 @@ if [ -z "$KEY_STORE_PASSWORD" ]; then
 	exit 1
 fi
 
+# Changes related to:AAI-2176
+# Change aai datarouter  container processes to run as non-root on the host
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001}
+DR_LOGS=/var/log/onap/AAI-DR
+
+if [ $(cat /etc/passwd | grep aaiadmin | wc -l) -eq 0 ]; then
+
+        groupadd aaiadmin -g ${GROUP_ID} || {
+                echo "Unable to create the group id for ${GROUP_ID}";
+                exit 1;
+        }
+        useradd --shell=/bin/bash -u ${USER_ID} -g ${GROUP_ID} -o -c "" -m aaiadmin || {
+                echo "Unable to create the user id for ${USER_ID}";
+                exit 1;
+        }
+fi;
+chown -R aaiadmin:aaiadmin ${MICRO_HOME}
+chown -R aaiadmin:aaiadmin ${DR_LOGS}
+find ${MICRO_HOME}  -name "*.sh" -exec chmod +x {} +
+
+gosu aaiadmin ln -s /logs $MICRO_HOME/logs
+JAVA_CMD="exec gosu aaiadmin java";
+###
 PROPS="-DAJSC_HOME=${MICRO_HOME}"
 PROPS="$PROPS -Dlogging.config=${MICRO_HOME}/bundleconfig/etc/logback.xml"
 PROPS="$PROPS -DCONFIG_HOME=$CONFIG_HOME"
@@ -33,4 +57,4 @@ JVM_MAX_HEAP=${MAX_HEAP:-1024}
 cd ${MICRO_HOME}
 jar uf0 ${MICRO_HOME}/data-router.jar BOOT-INF/lib/* > /dev/null 2>&1
 
-exec java -Xmx${JVM_MAX_HEAP}m $PROPS -jar ${MICRO_HOME}/data-router.jar
+${JAVA_CMD}  -Xmx${JVM_MAX_HEAP}m $PROPS -jar ${MICRO_HOME}/data-router.jar
