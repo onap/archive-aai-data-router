e1ca97f1cff72a7ad1b7fe838a3f15a3930d11ae
wr148d <wr148d@att.com>
2020-06-01 14:03:35 -0400

    Containers should not run as root by default
    Issue-ID: AAI-2822
    Signed-off-by: wr148d <wr148d@att.com>
    Change-Id: I4ddab6247bc8b4a44d62023b4fec0c579838f9ed

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -14,29 +14,9 @@ if [ -z "$KEY_STORE_PASSWORD" ]; then
 	exit 1
 fi
 
-# Changes related to:AAI-2176
-# Change aai datarouter  container processes to run as non-root on the host
-USER_ID=${LOCAL_USER_ID:-9001}
-GROUP_ID=${LOCAL_GROUP_ID:-9001}
-DR_LOGS=/var/log/onap/AAI-DR
-
-if [ $(cat /etc/passwd | grep aaiadmin | wc -l) -eq 0 ]; then
-
-        groupadd aaiadmin -g ${GROUP_ID} || {
-                echo "Unable to create the group id for ${GROUP_ID}";
-                exit 1;
-        }
-        useradd --shell=/bin/bash -u ${USER_ID} -g ${GROUP_ID} -o -c "" -m aaiadmin || {
-                echo "Unable to create the user id for ${USER_ID}";
-                exit 1;
-        }
-fi;
-chown -R aaiadmin:aaiadmin ${MICRO_HOME}
-chown -R aaiadmin:aaiadmin ${DR_LOGS}
 find ${MICRO_HOME}  -name "*.sh" -exec chmod +x {} +
 
-gosu aaiadmin ln -s /logs $MICRO_HOME/logs
-JAVA_CMD="exec gosu aaiadmin java";
+JAVA_CMD="exec java";
 ###
 PROPS="-DAJSC_HOME=${MICRO_HOME}"
 PROPS="$PROPS -Dlogging.config=${MICRO_HOME}/bundleconfig/etc/logback.xml"
